34e3c595125fa67c3043601ade2dc7966b384cb6
wanghongwei2 <wanghongwei2@100tal.com>
2020-07-16 20:52:57 +0800

    finish

--- read.go
+++ read.go
@@ -4,74 +4,126 @@ import (
 	"bytes"
 	"fmt"
 	"os"
+	"runtime"
 )
 
-const initReadSize = 2<<16
-func ReadLastLine(fileName string)(data []byte,err error)  {
+const initReadSize = 2 << 4
+
+func ReadLastLine(fileName string) (data []byte, err error) {
 	var f *os.File
-	f,err = os.Open(fileName)
-	if err != nil{
+	f, err = os.Open(fileName)
+	if err != nil {
 		return
 	}
 
 	var info os.FileInfo
-	info,err = f.Stat()
-	if err != nil{
+	info, err = f.Stat()
+	if err != nil {
 		return
 	}
 
-	if info.IsDir(){
-		err = fmt.Errorf("invalid file, name:%s",fileName)
+	if info.IsDir() {
+		err = fmt.Errorf("invalid file, name:%s", fileName)
+		return
 	}
 
-	return 	read(f,info.Size())
+	return read(f, info.Size())
 }
 
-func read(f *os.File, fileSize int64) (data []byte,err error) {
-	var bufIndex []int
+func read(f *os.File, fileSize int64) (data []byte, err error) {
+	var piecesLengthArray []int64
 	var buf = bytes.Buffer{}
 
-	var i,sepIndex int
-	var offset,sizeWillRead, readSize int64
+	var sepIndex int
+	var offset, sizeWillRead, sizeHasRead int64
 	var b []byte
 	var sep = getLineBreak()
 	sizeWillRead = initReadSize
 
+	if fileSize < int64(len(sep)) {
+		data = make([]byte, fileSize)
+		_, err = f.ReadAt(b, 0)
+		return
+	}
+
+	//ignore the last line break if exists
+	b = make([]byte, len(sep))
+	if _, err = f.ReadAt(b, fileSize-int64(len(sep))); err != nil {
+		return nil, err
+	} else if bytes.Equal(b, sep) {
+		fileSize -= int64(len(sep))
+	}
+
 	for {
-		offset = fileSize-(sizeWillRead+readSize)
-		if offset < 0{
-			sizeWillRead = fileSize-readSize
+		//fmt.Println(piecesLengthArray)
+		sizeWillRead = sizeWillRead << 1
+		offset = fileSize - (sizeWillRead + sizeHasRead)
+		if offset < 0 {
+			sizeWillRead = fileSize - sizeHasRead
 			offset = 0
 		}
 
+		sizeHasRead += sizeWillRead
 		b = make([]byte, sizeWillRead)
-		_,err = f.ReadAt(b,offset)
-		if err != nil{
+		_, err = f.ReadAt(b, offset)
+		if err != nil {
 			return
 		}
 
-		sepIndex = findSep(b,sep)
-		if sepIndex == -1{
-			bufIndex = append(bufIndex, int(readSize))
+		//fmt.Println( "length: ",len(b), string(b))
+		sepIndex = findSep(b, sep)
+		if sepIndex == -1 {
+			piecesLengthArray = append(piecesLengthArray, sizeWillRead)
+			buf.Write(b)
+			if sizeHasRead >= fileSize {
+				return bytesReassemble(buf.Bytes(), piecesLengthArray), nil
+			}
 			continue
 		}
+		piecesLengthArray = append(piecesLengthArray, int64(len(b)-sepIndex-len(sep)))
+		buf.Write(b[sepIndex+len(sep):])
+		//fmt.Println(  "sepIndex:", sepIndex)
+		//fmt.Println("bytes: ",string(buf.Bytes()))
 
-		b = buf.Bytes()
-		data = make([]byte, len(b))
-		var length = len(bufIndex)
-		var start,end int
-		for i=length-1;i>=0;i--{
-			copy(data[start:end], b[start:end])
-		}
-
-		return
+		return bytesReassemble(buf.Bytes(), piecesLengthArray), nil
 	}
 }
 
 func findSep(s, sep []byte) int {
-	return bytes.LastIndex(s,sep)
+	return bytes.LastIndex(s, sep)
 }
 
 func getLineBreak() []byte {
-	return []byte("\n")
+	switch runtime.GOOS {
+	case "windows":
+		return []byte("\r\n")
+	default:
+		return []byte("\n")
+	}
+}
+
+// reassemble bytes array, like from [e,d,bc,a] to [a,bc,d,e]
+func bytesReassemble(b []byte, piecesLengthArray []int64) (data []byte) {
+	var length = int64(len(piecesLengthArray))
+	var bytLength = int64(len(b))
+	var sum int64
+	for _, piecesLength := range piecesLengthArray {
+		sum += piecesLength
+	}
+
+	if sum != bytLength {
+		panic(fmt.Errorf("byes sum is not matched with sum of piecesLengthArray, %d != %d", bytLength, sum))
+	}
+	data = make([]byte, bytLength)
+	var oldStart, start, subLength int64
+
+	for i := length - 1; i >= 0; i-- {
+		subLength = piecesLengthArray[i]
+		oldStart = bytLength - subLength
+		copy(data[start:start+subLength], b[oldStart:oldStart+subLength])
+		start += subLength
+		bytLength -= subLength
+	}
+
+	return data
 }
---
+++ read_test.go
@@ -0,0 +1,9 @@
+package read_file_last_line
+
+import (
+	"testing"
+)
+
+func TestReadLastLine(t *testing.T) {
+
+}
